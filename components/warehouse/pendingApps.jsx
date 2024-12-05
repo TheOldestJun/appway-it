7ed5846de9a53bf355a4aa206f85ef890d3842dd
Stanislav <s.v.pukha@gmail.com>
2024-12-05 15:51:44 +0200

    warehouse UI init

--- components/warehouse/pendingApps.jsx
+++ components/warehouse/pendingApps.jsx
@@ -1,5 +1,32 @@
+import { useGetNotReceivedQuery, useSetReceivedMutation } from "@/store/services/orders"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
 export default function PendingApps() {
     return (
-        <div>Pending Apps</div>
+        <Table>
+            <TableCaption>Очікується поставка</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Дата заявки</TableHead>
+                    <TableHead>Заявник</TableHead>
+                    <TableHead>Назва ТМЦ</TableHead>
+                    <TableHead>Примітки</TableHead>
+                    <TableHead className="text-right">Од. вим.</TableHead>
+                    <TableHead className="text-right">Замовлено</TableHead>
+                    <TableHead className="text-right">Отримати</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {/* {mappedData} */}
+            </TableBody>
+        </Table>
     )
 }
